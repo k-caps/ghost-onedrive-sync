6d16e3b95f55418272671c75d02a5a2badb392aa
Kobi <18639419+k-caps@users.noreply.github.com>
2025-04-06 20:10:46 +0300

    added comments to pick up from

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 
 
+def get_this_months_photos(onedrive_photos_list):
+    this_month = datetime.datetime.now().strftime("%Y%m")
+    this_months_photos = []
+
+    for photo in onedrive_photos_list:
+        print('Photo name: ' + photo["name"])
+        if photo["name"].startswith(this_month):
+            this_months_photos.append(photo["name"])
+
+    return this_months_photos
+
+
+def check_previously_synced_photos():
+    pass
+
+
+def sync_failed_photos():
+    pass
+
 
 def compress_images():
     pass
@@ -43,13 +62,18 @@ def main():
         onedrive.interactive_login(msal_app, config["scopes"], config["token_cache_path"])
 
     onedrive_photos_list = onedrive.get_photos_list(access_token, config['onedrive_endpoint'])
-    this_month = datetime.datetime.now().strftime("%Y%m")
-    this_months_photos = []
 
-    for photo in onedrive_photos_list:
-        print('Photo name: ' + photo["name"])
-        if photo["name"].startswith(this_month):
-            this_months_photos.append(photo["name"])
+    # Don't always assume that every previous run synced every file sucessfully.
+    # We need to maintain a list of photos that have been targeted to be synced previously, then check if any of those failed.
+    # aside from failed atttempts, also go through this month's photos and check if they have been synced.
+    # maintain a file with all photos marked for sync and also those failed?
+    # basically ensure that we know the state of any given photo? Oof.
+
+    this_months_photos = get_this_months_photos(onedrive_photos_list)
+    write_photos_to_sync_list(this_months_photos)
+
+
+
             
     
 
