245389d3c7838ac8eb6fa88f8049ddf0e242ccf6
Kobi <18639419+k-caps@users.noreply.github.com>
2025-04-06 19:30:00 +0300

    start work on parsing onedrive api response

--- main.py
+++ main.py
@@ -48,16 +48,20 @@ def main():
     access_token = onedrive.get_access_token(msal_app, config["scopes"], config["token_cache_path"])
     if not access_token:
         onedrive.interactive_login(msal_app, config["scopes"], config["token_cache_path"])
-    onedrive_photos_list = onedrive.get_photos_list(access_token, config['onedrive_endpoint'])
-    this_month = datetime.datetime.now().strftime("%Y%m")
-    this_months_photos = []
+    all_onedrive_files_json = onedrive.list_onedrive_files(access_token, config['onedrive_endpoint'])
+    print(json.dumps(all_onedrive_files_json))
+
+
+    #onedrive_photos_list = onedrive.get_photos_list(access_token, config['onedrive_endpoint'])
+    #this_month = datetime.datetime.now().strftime("%Y%m")
+    #this_months_photos = []
 
     # seems that only files from 2023 are being returned. Maybe the API only returns chunks?
-    for photo in onedrive_photos_list:
-        #print(photo["name"])
-        if photo["name"].startswith(this_month):
-            #print(photo["name"])
-            pass
+    #for photo in onedrive_photos_list:
+    #    #print(photo["name"])
+    #    if photo["name"].startswith(this_month):
+    #        #print(photo["name"])
+    #        pass
         
     #print(this_month)
     #for photo in this_months_photos:
